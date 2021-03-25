4c747565cb09774ccbfbeab9ffec459bc6fa757a
jgruenbaum <jeff.gruenbaum@gmail.com>
2021-03-25 15:02:09 -0500

    added pyproject.toml and setup.py, removed unnecessary pypi packages

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from setuptools import find_packages, setup
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name = 'encryptoenv',         # How you named your package folder (MyLib)
+    packages = find_packages(exclude=("tests",)),
+    version = '0.0.1',      # Start with a small number and increase it with every change you make
+    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description = 'With one command, you can setup and protect your environment variables.',   # Give a short description about your library
+    author = 'Jeff Gruenbaum',                   # Type in your name
+    author_email = 'jeff.gruenbaum@gmail.com',      # Type in your E-Mail
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/jgrugru/encrypto-env',   # Provide either the link to your github or to your website
+    keywords = ["encryption", "environment_variables", "env", "dotenv", "python", "environment"],
+    install_requires=[
+        "pycryptodome==3.10.1",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
