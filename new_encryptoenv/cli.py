071a481166bfb02fd907f451b91d0679c22487cb
jgrugru <jeff.gruenbaum@gmail.com>
2022-02-19 16:37:40 -0600

    adding precommit, black and flake8

--- new_encryptoenv/cli.py
+++ new_encryptoenv/cli.py
@@ -1,11 +1,9 @@
 import click
-import os
-import sys
-import typing
 from dotenv import find_dotenv
 
 
 VERSION = "1.0.0"
+
 
 @click.command()
 @click.version_option(version=VERSION)
@@ -14,6 +12,7 @@ def main():
     """Interact with your encrypted .env file."""
     env_filepath = find_dotenv(raise_error_if_not_found=True)
     print(env_filepath)
+
 
 if __name__ == "__main__":
     main()
--- new_encryptoenv/dotenv.py
+++ new_encryptoenv/dotenv.py
@@ -2,12 +2,13 @@ import typing
 import os
 import sys
 
+
 def _walk_to_root(path: str) -> typing.Iterator[str]:
     """
     Yield directories starting from the given directory up to the root
     """
     if not os.path.exists(path):
-        raise IOError('Starting path not found')
+        raise IOError("Starting path not found")
 
     if os.path.isfile(path):
         path = os.path.dirname(path)
@@ -19,18 +20,21 @@ def _walk_to_root(path: str) -> typing.Iterator[str]:
         parent_dir = os.path.abspath(os.path.join(current_dir, os.path.pardir))
         last_dir, current_dir = current_dir, parent_dir
 
-def find_dotenv(filename: str = '.env', raise_error_if_not_found: bool = False, usecwd: bool = False) -> str:
+
+def find_dotenv(
+    filename: str = ".env", raise_error_if_not_found: bool = False, usecwd: bool = False
+) -> str:
     """
     Search in increasingly higher folders for the given file
     Returns path to the file if found, or an empty string otherwise
     """
 
     def _is_interactive():
-        """ Decide whether this is running in a REPL or IPython notebook """
-        main = __import__('__main__', None, None, fromlist=['__file__'])
-        return not hasattr(main, '__file__')
+        """Decide whether this is running in a REPL or IPython notebook"""
+        main = __import__("__main__", None, None, fromlist=["__file__"])
+        return not hasattr(main, "__file__")
 
-    if usecwd or _is_interactive() or getattr(sys, 'frozen', False):
+    if usecwd or _is_interactive() or getattr(sys, "frozen", False):
         # Should work without __file__, e.g. in REPL or IPython notebook.
         path = os.getcwd()
     else:
@@ -50,6 +54,6 @@ def find_dotenv(filename: str = '.env', raise_error_if_not_found: bool = False, 
             return check_path
 
     if raise_error_if_not_found:
-        raise IOError('File not found')
+        raise IOError("File not found")
 
-    return ''
+    return ""
